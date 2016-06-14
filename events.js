ba0c6b13ed2409fc64f38912ec3f68dd3a4d1070
Mike Meadows <mmeadow33@gmail.com>
2016-06-14 17:17:30 -0500

    events.js are starting to work

--- events.js
+++ events.js
@@ -1,12 +1,19 @@
-var CarLot = (function (event) {
-	var car = document.getElementById("carClass")
-	var input = document.getElementById("input")
-	
-	event.activateEvents = function () {
-		var currentCard;
+// When you click on one of the car elements, change the width of the border to a higher value,
+//  and change the background color to any other color of your choosing.
+// Also, on click of the car element, clear the value of the text input 
+// in the navbar, and put the cursor in the text input.
+// When you start typing into the navbar's text input, the description of the 
+// currently selected car should be bound to what you are typing in and match it exactly.
 
-	}
+var CarLot = (function (newCarLot) {
+  var selectedCar = document.getElementsByClassName("carClass");
+  var inputText = document.getElementById("inputText");
+  console.log(selectedCar)
 
 
+for (var j = 0; j < selectCar.length; j++){
 
-});
+
+return newCarLot;
+
+}})(CarLot || {});
--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@ function populatePage (inventory) {
 
   for (var i = 0; i < inventory.cars.length; i++) { 
     var currentCar = inventory.cars[i];
-    inventorylist += `<div id="card--${i}" class="carClass" style="border: 1px solid ${currentCar.color}">
+    inventorylist += `<div id="car--${i}" class="carClass" style="border: 1px solid ${currentCar.color}">
     <h2>${currentCar.make} ${currentCar.model}</h2>
     <h4>${currentCar.year}  $${currentCar.price}</h4>
     <h4>${currentCar.color}  ${currentCar.purchased}</h4>
