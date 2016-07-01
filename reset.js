873a1016eac25467a76bf647e8fe3311a5c7120c
Mike Meadows <mmeadow33@gmail.com>
2016-06-30 21:53:02 -0500

    changeing between elements now works correctly

--- reset.js
+++ reset.js
@@ -4,16 +4,16 @@ var CarLot = (function (newCarlot) {
 	newCarlot.resetBorder = function() {
 var editedCars = document.getElementsByClassName("cars");
 		for (var i = 0; i < editedCars.length; i++) {
-			editedCars[i].classList.remove("newCar");
-			editedCars[i].classList.add("carClass");
-			// console.log("hello")
-		}
+			editedCars[i].classList.remove("carClass");
+			// editedCars[i].classList.add("carClass");
+			}
 	};
 
 ////////Changes border to assigned color in functions argument//////////
+	
 	newCarlot.changeBorder = function (event, color) {
-       event.classList.remove("carClass");
-       event.classList.add("newClass");//// 
+       // event.classList.remove("carClass");
+       event.classList.add("carClass");//// 
  	   // event.style.backgroundColor = color;
  	   // console.log("hey")
  }
